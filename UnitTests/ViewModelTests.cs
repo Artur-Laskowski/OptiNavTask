277a025da4bd2b6526d19a8b056dadae717ff329
Artur-Laskowski <laskowski.artur.96@gmail.com>
2018-07-03 01:15:43 +0200

    added unit tests

---
+++ UnitTests/ViewModelTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImageProcessingApp;
+using ImageProcessingLibrary;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using Moq;
+
+namespace UnitTests {
+    [TestClass]
+    public class ViewModelTests {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void LoadImage_IncorrectPath_ArgumentException() {
+            // arrange
+            var path = @"incorrectPath";
+            var ivm = new ImageViewModel();
+            ivm.ImagePath = path;
+
+            // act
+            ivm.LoadImageCommand.Execute(null);
+
+            // assert
+            //exception
+        }
+        
+        [TestMethod]
+        public void LoadImage_PathIsEmpty_PropertiesNotSet() {
+            //arrange
+            var ivm = new ImageViewModel();
+
+            //act
+            ivm.LoadImageCommand.Execute(null);
+
+            //assert
+            Assert.AreEqual(null, ivm.ImageHeight);
+            Assert.AreEqual(null, ivm.ImageWidth);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void Grayscale_ImageNotSet_Exception() {
+            //arrange
+            var ivm = new ImageViewModel();
+
+            //act
+            ivm.GrayscaleCommand.Execute(null);
+            
+            //assert
+            //exception
+        }
+    }
+}
