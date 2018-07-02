6e4000567cd87cd432fd7e055bfde6152d845a2a
Artur-Laskowski <laskowski.artur.96@gmail.com>
2018-07-02 18:11:59 +0200

    Added grayscale function that works with encoded images.

--- ImageProcessingLibrary/ImageProcessing.cs
+++ ImageProcessingLibrary/ImageProcessing.cs
@@ -20,7 +20,6 @@ namespace ImageProcessingLibrary
         public int TasksCount { get; set; }
 
         private RawDataConverter RawDataManager;
-        
 
         public double RedRatio { get; private set; }
         public double GreenRatio { get; private set; }
@@ -52,10 +51,19 @@ namespace ImageProcessingLibrary
 
         private void LoadImageAsBitmap(string filePath) {
             ProcessedImage = new Bitmap(filePath);
+
+            int depth = Image.GetPixelFormatSize(ProcessedImage.PixelFormat);
+            if (depth != 24 && depth != 32) {
+                throw new ArgumentException("Only 24 and 32 bpp images are supported.");
+            }
         }
         
         public void SaveImage(string newFilePath) {
             RawDataManager.SaveRawDataToBitmap(ProcessedImage);
+            ProcessedImage.Save(newFilePath);
+        }
+
+        public void SaveEncodedImage(string newFilePath) {
             ProcessedImage.Save(newFilePath);
         }
 
@@ -67,9 +75,12 @@ namespace ImageProcessingLibrary
         public async Task GrayscaleAsync() {
             int totalLength = ProcessedImage.Height * ProcessedImage.Width;
             int chunkLength = totalLength / TasksCount;
-            //TODO: handle image height < TasksCount
 
-            Task[] tasks = new Task[TasksCount];
+            int numberOfTasks = TasksCount;
+            if (ProcessedImage.Height < TasksCount)
+                numberOfTasks = 1;
+
+            Task[] tasks = new Task[numberOfTasks];
 
             for (int i = 0; i < TasksCount; i++) {
                 int startIndex = i * chunkLength;
@@ -83,6 +94,21 @@ namespace ImageProcessingLibrary
             }
 
             await Task.WhenAll(tasks);
+        }
+
+        public void GrayscaleEncodedImage() {
+            //RawDataManager.SaveRawDataToBitmap(ProcessedImage);
+            for (int x = 0; x < ProcessedImage.Width; x++) {
+                for (int y = 0; y < ProcessedImage.Height; y++) {
+                    Color pixel = ProcessedImage.GetPixel(x, y);
+                    double r = pixel.R * RedRatio;
+                    double g = pixel.G * GreenRatio;
+                    double b = pixel.B * BlueRatio;
+                    byte gray = (byte)(r + g + b);
+                    Color newColor = Color.FromArgb(gray, gray, gray);
+                    ProcessedImage.SetPixel(x, y, newColor);
+                }
+            }
         }
         
         public void ProcessSegment(int startIndex, int length) {
